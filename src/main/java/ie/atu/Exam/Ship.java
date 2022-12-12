b65ead6da050deb0c42e1f5a10f546239eed1628
Gavin Walsh <G00402296@atu.ie>
2022-12-12 09:11:13 +0000

    Added Vessal app and Getter + setters for Ship class

--- src/main/java/ie/atu/Exam/Ship.java
+++ src/main/java/ie/atu/Exam/Ship.java
@@ -1,4 +1,43 @@
 package ie.atu.Exam;
 
 public class Ship {
+    private String shipName;
+    private String shipAge;
+    private int crewNum;
+
+    public Ship() {
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getShipAge() {
+        return shipAge;
+    }
+
+    public void setShipAge(String shipAge) {
+        this.shipAge = shipAge;
+    }
+
+    public int getCrewNum() {
+        return crewNum;
+    }
+
+    public void setCrewNum(int crewNum) {
+        this.crewNum = crewNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Ship{" +
+                "shipName='" + shipName + '\'' +
+                ", shipAge='" + shipAge + '\'' +
+                ", crewNum=" + crewNum +
+                '}';
+    }
 }
